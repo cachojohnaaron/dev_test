449e14bf564f7fefd04f75af31ee50bb4b5de0de
cachojohnaaron <95965089+cachojohnaaron@users.noreply.github.com>
2022-05-04 10:43:40 +0800

    Add files via upload

--- Api/db.php
+++ Api/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost","apps.ojt", "Ojt@apps2022!", "ims");
+$conn = new mysqli("localhost","u676613437_ims", "Aaroncacho123", "u676613437_ims");
 if(!$conn){
     echo "Error. Database not found.";
 }
